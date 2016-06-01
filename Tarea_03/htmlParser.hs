7f9e14d3c0f567c4eaf518216f6a8c22e8deb2d6
Gustavo <gustavogut1993@gmail.com>
2016-06-01 01:49:11 +0000

    Terminado el parser, falta probar

--- Tarea_03/htmlParser.hs
+++ Tarea_03/htmlParser.hs
@@ -4,49 +4,78 @@ import Text.ParserCombinators.Parsec
 lhs :: Parser String
 lhs = do  elems <- many element
           eof
-          return (concat elems)
+          return $ concat elems
 
 element :: Parser String
-element = h1 <|> h2 <|> code <|> eol -- <|> paragraph 
+element = eol <|> code <|> try(h1) <|> try(h2) <|> paragraph 
           
 h1 :: Parser String
-h1 = do char '*'
-        cont <- many $ noneOf "\n"
-        eol
-        return ("<h1>" ++ cont ++ "</h1>"++"\n")
+h1 = do whitespace
+        char '*'
+        cont <- many $ validChar
+        eol <?> "expecting end of line to finish h1 block."
+        return $ "<h1>" ++ concat cont ++ "</h1>"++"\n"
 
 h2 :: Parser String
-h2 = do char '#'
-        cont <- many $ noneOf "\n"
-        eol
-        return ("<h2>" ++ cont ++ "</h2>"++"\n")
+h2 = do whitespace
+        char '#'
+        cont <- many $ validChar
+        eol <?> "expecting end of line to finish h2 block."
+        return $ "<h2>" ++ concat cont ++ "</h2>"++"\n"
 
 code :: Parser String
 code = do cont <- many1 $ codeLine
-          return ("<code>" ++ unlines cont ++ "</code>" ++ "\n")
+          return $ "<code>\n" ++ unlines cont ++ "</code>" ++ "\n"
           
 codeLine :: Parser String
 codeLine = do string "> "
-              cont <- many $ noneOf "\n"
-              eol 
-              return cont
+              cont <- many $ validChar
+              eol <?> "expecting end of line to finish codeLine." 
+              return $ concat cont
+              
+paragraph :: Parser String
+paragraph = do  cont <- many parLine
+                eol <?> "expecting end of line to finish paragraph."
+                return $ "<p>\n" ++ unlines cont ++ "</p>" ++ "\n"
 
+parLine :: Parser String
+parLine = do  cont <- many1 validChar
+              eol <?> "expecting end of line to finish parLine."
+              return $ concat cont
 
-        
--- paragraph :: Parser String
--- paragraph = do  cont <- many noBlankLine
---                 blankLine
---                 return ("<p>" ++ unlines cont ++ "<\p>")
-                
--- noBlankLine :: Parser String
--- noBlankLine = do  cont <- many $ noneOf "\n"
---                   eol
+validChar :: Parser String
+validChar = do  c <- noneOf "\n"
+                case c of
+                  '&' -> return "&amp;"
+                  '<' -> return "&lt;"
+                  '>' -> return "&gt;"
+                  ' ' -> whitespace
+                  otherwise -> return [c]
+
+whitespace :: Parser String
+whitespace = do many $ char ' '
+                return " "
 
 eol :: Parser String
 eol = do  char '\n'
-          return ""
+          return  ""
+
+lhsName :: Parser String
+lhsName = do  cont <- manyTill anyChar $ string ".lhs"
+              eof
+              return cont
+
+parseLHS :: String -> IO ()
+parseLHS file = do  input <- readFile file
+                    case parse lhs file input of
+                      Left e -> do  putStr "Error: "
+                                    print e
+                      Right r -> case parse lhsName ".html" file of
+                          Left e -> do  putStr "FileNameError: "
+                                        print e
+                          Right s -> writeFile (s ++ ".html") r
 
 main = do
-        [f] <- getArgs
-        cont <- readFile f 
-        print $ parse lhs "prueba" cont
+        files <- getArgs
+        mapM_ parseLHS files 
+        -- print $ parse lhs "prueba" cont
--- Tarea_03/prueba.lhs
+++
@@ -1,8 +0,0 @@
-* Esto es un encabezado
-# Esto es un H2
-> codigo1
-> codigo1.1
-
-> codigo2
-> codigo2
-
